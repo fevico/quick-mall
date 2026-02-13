b1b64492c40cea9050c6e79d828138bae7443cdf
fevico <ajayifavour81@gmail.com>
2026-02-13 12:52:12 +0100

    auth module setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+import { PrismaClient } from 'prisma/generated/client';
 
 @Injectable()
-export class PrismaService {
-    
+export class PrismaService extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    this.logger.log('Connecting to the database...');
+    await this.$connect();
+    this.logger.log('Connected to the database successfully!');
+  }
+
+  async onModuleDestroy() {
+    this.logger.log('Disconnecting from the database...');
+    await this.$disconnect();
+    this.logger.log('Disconnected from the database.');
+  }
 }
